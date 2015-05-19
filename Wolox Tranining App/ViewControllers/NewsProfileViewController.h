90083e949bd4433e838749dca9cad52eed0e0235
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2015-05-19 09:50:58 -0300

    News view controller and xibs.

---
+++ Wolox Tranining App/ViewControllers/NewsProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsProfileViewController.h
+//  Wolox Tranining App
+//
+//  Created by Francisco Depascuali on 5/5/15.
+//  Copyright (c) 2015 Francisco Depascuali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsProfileViewController : UITabBarController
+
+@end
