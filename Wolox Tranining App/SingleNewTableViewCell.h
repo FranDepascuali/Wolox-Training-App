0c22b1cdfbc9d0d38f69b971263fcee6cc8d690a
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2015-05-19 09:50:58 -0300

    News view controller and xibs.

---
+++ Wolox Tranining App/SingleNewTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  NewCellTableTableViewCell.h
+//  Wolox Tranining App
+//
+//  Created by Francisco Depascuali on 5/5/15.
+//  Copyright (c) 2015 Francisco Depascuali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleNewTableViewCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *user;
+@property (nonatomic, weak) IBOutlet UIImageView *userImage;
+@property (weak, nonatomic) IBOutlet UILabel *desc;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+
+
+@end
---
+++ Wolox Tranining App/ViewControllers/NewsCollectionViewController.m
@@ -0,0 +1,79 @@
+//
+//  NewsViewController.m
+//  Wolox Tranining App
+//
+//  Created by Francisco Depascuali on 5/5/15.
+//  Copyright (c) 2015 Francisco Depascuali. All rights reserved.
+//
+
+#import "NewsCollectionViewController.h"
+#import "NewsViewModel.h"
+#import "SingleNewTableViewCell.h"
+#import "NewsCollectionViewModel.h"
+#import "NewsViewModel.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+#define CELL_ID @"NewsTableViewCell"
+
+@interface NewsCollectionViewController ()
+
+@property (nonatomic,strong) NewsCollectionViewModel* model;
+
+@end
+
+@implementation NewsCollectionViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	[self.loadingTableIndicator setHidesWhenStopped:YES];
+	[self.loadingTableIndicator startAnimating];
+	self.model = [NewsCollectionViewModel new];
+	[self.newsTable registerNib:[UINib nibWithNibName:CELL_ID bundle:nil] forCellReuseIdentifier:CELL_ID];
+	self.newsTable.delegate=self;
+	self.newsTable.dataSource=self;
+	[self.model fetchNewsWithSuccess:^ {
+		[self.newsTable reloadData];
+		[self.loadingTableIndicator stopAnimating];
+	}error:^(NSString * err) {
+		[self.loadingTableIndicator stopAnimating];
+		[self displayError:err];
+	}];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.model count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    SingleNewTableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    NewsViewModel* newViewModel = [self.model newsViewModelAtIndex:indexPath.row];
+    [newViewModel fetchUserNameWithSuccess:^(NSString * name) {
+        cell.user.text = name;
+    } error:^(NSString* err) {
+        [self displayError:err];
+    }];
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString: [newViewModel getImage]]
+                      placeholderImage:[UIImage imageNamed:@"header_logo"]];
+    cell.likeButton.tag = indexPath.row;
+    [cell.likeButton setImage:[self.model likeImageAtIndex: indexPath.row] forState:UIControlStateNormal];
+	[cell.likeButton addTarget:self action:@selector(likeButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    cell.desc.text = [newViewModel getDescription];
+    return cell;
+}
+
+- (void)likeButtonClicked:(UIButton*)btn {
+    [self.model like: btn.tag];
+    [btn setImage:[self.model likeImageAtIndex:btn.tag] forState:UIControlStateNormal];
+}
+
+#pragma mark - Private
+
+- (void)displayError:(NSString *)error {
+    [self.view makeToast:error];
+}
+
+@end
